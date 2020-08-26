a3d836bf5569e9163d57f35e995a993dbb07c837
Onwugbenu Chisom <onwugbenuglory@gmail.com>
2020-08-26 10:16:13 +0100

    my first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    populateCountries("country", "state");
+    
+    $countryElement = $('#country');
+    $stateElement = $('#state');
+    
+    $countryElement.val('USA').trigger('change');
+    $stateElement.val('Florida');
+    
+    setTimeout(function() {
+      $countryElement.val('American Samoa').trigger('change');
+      $stateElement.val('Eastern');
+    }, 5000);
+    
+    });
+
+    function validateform(){  
+        const input=document.myform.name.value;  
+
+    let today = new Date();
+    let submission = new Date(2020, 7, 29, 12, 00);
+    let deadline = new Date(2020, 7, 31, 11, 59);
+          
+        if (name ==null || name ==" "){  
+          alert("Fill in all fields");  
+          
+        }
+        if (today <= submission && today <= deadline) {
+          alert('Registration successful.');
+          return true;
+      } else {
+          alert('Registration has ended');
+          return false;
+      }
+        }  
+
+
+
+
